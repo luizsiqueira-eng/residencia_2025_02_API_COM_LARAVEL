fa67ec51335a9d358e0043af8489eb2766e98877
Luís Eduardo <luiseduardocass06@gmail.com>
2025-10-13 04:59:20 -0300

    fix: Revert new enum implementation (PHP 8.1) to be PHP 8.0 compatible

---
+++ src/app/Enums/ConteudoStatusEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Enums;
+
+class ConteudoStatusEnum {
+    public const ESCRITO = "escrito";
+    public const APROVADO = "aprovado";
+    public const REPROVADO = "reprovado";
+
+}
