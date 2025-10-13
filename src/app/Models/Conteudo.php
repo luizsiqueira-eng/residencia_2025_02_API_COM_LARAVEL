ea07e834ebc2abbe57e47113324fe097b28fb1d3
Luís Eduardo <luiseduardocass06@gmail.com>
2025-10-13 15:47:10 -0300

    Feat: Implements aprovar conteudo, reprovar conteudo and turn back reprovado status to escrito after edited

--- src/app/Models/Conteudo.php
+++ src/app/Models/Conteudo.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\ConteudoStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Exception;
 
 class Conteudo extends Model
 {
@@ -15,4 +17,50 @@ class Conteudo extends Model
         "status",
         "motivo_reprovacao"
     ];
+
+    protected $casts = [
+        'status' => ConteudoStatusEnum::Enum,
+    ];
+
+    protected $attributes = [
+        'status'=> ConteudoStatusEnum::ESCRITO,
+    ];
+
+     public function aprovar() {
+
+        if ($this->status !== ConteudoStatusEnum::ESCRITO) {
+            throw new Exception('Ação de reprovar não permitida para o estado atual.');
+        }
+
+        $this->status = ConteudoStatusEnum::APROVADO;
+    
+        return $this->save(); 
+     }
+
+    public function reprovar(string $motivo_reprovacao) {
+
+        if ($this->status !== ConteudoStatusEnum::ESCRITO) {
+            throw new Exception('Ação de reprovar não permitida para o estado atual.');
+        }
+        if (empty($motivo_reprovacao)) {
+            throw new Exception('O motivo da reprovação é obrigatório.');
+        }
+        
+        $this->status = ConteudoStatusEnum::REPROVADO;
+        $this->motivo_reprovacao = $motivo_reprovacao;
+
+        return $this->save();
+    }
+
+    public function statusEscritoAposEditarConteudoReprovado () {
+        if ($this->status !== ConteudoStatusEnum::REPROVADO) {
+            throw new Exception('O Conteúdo não foi reprovado, ação não disponível.');
+        }
+
+        $this->status = ConteudoStatusEnum::ESCRITO;
+        $this->motivo_reprovacao = null;
+
+        return $this->save();
+    }
+
 }
