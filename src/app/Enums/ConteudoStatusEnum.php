3f98608f7227ba4a83269b46feb07bf83f80092f
Luís Eduardo <luiseduardocass06@gmail.com>
2025-10-13 15:20:02 -0300

    fix: Fix enum syntax

--- src/app/Enums/ConteudoStatusEnum.php
+++ src/app/Enums/ConteudoStatusEnum.php
@@ -3,7 +3,9 @@
 
 namespace App\Enums;
 
-class ConteudoStatusEnum {
+use Illuminate\Validation\Rules\Enum;
+
+class ConteudoStatusEnum extends Enum {
     public const ESCRITO = "escrito";
     public const APROVADO = "aprovado";
     public const REPROVADO = "reprovado";
