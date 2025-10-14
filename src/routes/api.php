8422fd013d0eddcc1c24d0d6b0cb418ecbeb1dbd
Luís Eduardo <luiseduardocass06@gmail.com>
2025-10-14 02:42:21 -0300

    feat: add new api routes (aprovar and reprovar)

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\TokenController;
 use App\Http\Controllers\ConteudoController;
 
 
-
+//Crud basico
 Route::get("/conteudos", [ConteudoController::class,"index"])->name("Listar conteúdos");
 Route::post("/conteudos", [ConteudoController::class,"store"])->name("Criar conteúdo");
 Route::get("/conteudos/{id}", [ConteudoController::class,"show"])->name("Listar apenas um conteúdo");
 Route::put("/conteudos/{id}", [ConteudoController::class,"update"])->name("Atualiza apenas um conteúdo");
 Route::delete("/conteudos/{id}", [ConteudoController::class,"destroy"])->name("Deleta um conteúdo");
 
+// rota para aprovar e reprovar
+Route::post("/conteudos/{conteudo}/aprovar", [ConteudoController::class,"aprovar"])->name("Aprovar conteúdo");
+Route::post("/conteudos/{conteudo}/reprovar", [ConteudoController::class,"reprovar"])->name("Reprovar conteúdo");
 
 
 
